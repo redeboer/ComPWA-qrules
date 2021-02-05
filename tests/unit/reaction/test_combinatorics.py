d24f4b747bf6d3154f7e69de4f851a345e28f8b8
Remco de Boer <redeboer@gmx.com>
2021-02-05 17:58:53 +0100

    test: check identical particle combinatorics (#482)

--- tests/unit/reaction/test_combinatorics.py
+++ tests/unit/reaction/test_combinatorics.py
@@ -13,8 +13,15 @@ from expertsystem.reaction.combinatorics import (
     _KinematicRepresentation,
     _safe_set_spin_projections,
     create_initial_facts,
+    perform_external_edge_identical_particle_combinatorics,
 )
-from expertsystem.reaction.topology import Topology, create_isobar_topologies
+from expertsystem.reaction.quantum_numbers import ParticleWithSpin
+from expertsystem.reaction.topology import (
+    Edge,
+    StateTransitionGraph,
+    Topology,
+    create_isobar_topologies,
+)
 
 
 @pytest.fixture(scope="session")
@@ -22,66 +29,6 @@ def three_body_decay() -> Topology:
     topologies = create_isobar_topologies(1, 3)
     topology = next(iter(topologies))
     return topology
-
-
-@pytest.mark.parametrize(
-    "final_state_groupings",
-    [
-        ["pi0", "pi0"],
-        [["pi0", "pi0"]],
-        [[["pi0", "pi0"]]],
-        ["gamma", "pi0"],
-        [["gamma", "pi0"]],
-        [[["gamma", "pi0"]]],
-    ],
-)
-def test_initialize_graph(
-    final_state_groupings,
-    three_body_decay: Topology,
-    particle_database: ParticleCollection,
-):
-    initial_facts = create_initial_facts(
-        three_body_decay,
-        initial_state=[("J/psi(1S)", [-1, +1])],
-        final_state=["gamma", "pi0", "pi0"],
-        particles=particle_database,
-        final_state_groupings=final_state_groupings,
-    )
-    assert len(initial_facts) == 4
-
-
-@pytest.mark.parametrize(
-    "initial_state, final_state",
-    [
-        (["J/psi(1S)"], ["gamma", "pi0", "pi0"]),
-        (["J/psi(1S)"], ["K+", "K-", "pi+", "pi-"]),
-        (["e+", "e-"], ["gamma", "pi-", "pi+"]),
-        (["e+", "e-"], ["K+", "K-", "pi+", "pi-"]),
-    ],
-)
-def test_generate_outer_edge_permutations(
-    initial_state,
-    final_state,
-    three_body_decay: Topology,
-    particle_database: ParticleCollection,
-):
-    initial_state_with_spins = _safe_set_spin_projections(
-        initial_state, particle_database
-    )
-    final_state_with_spins = _safe_set_spin_projections(
-        final_state, particle_database
-    )
-    list_of_permutations = list(
-        _generate_outer_edge_permutations(
-            three_body_decay,
-            initial_state_with_spins,
-            final_state_with_spins,
-        )
-    )
-    n_permutations_final_state = factorial(len(final_state))
-    n_permutations_initial_state = factorial(len(initial_state))
-    n_permutations = n_permutations_final_state * n_permutations_initial_state
-    assert len(list_of_permutations) == n_permutations
 
 
 class TestKinematicRepresentation:
@@ -159,6 +106,66 @@ class TestKinematicRepresentation:
             assert ["should be nested list"] in kinematic_representation
 
 
+@pytest.mark.parametrize(
+    "final_state_groupings",
+    [
+        ["pi0", "pi0"],
+        [["pi0", "pi0"]],
+        [[["pi0", "pi0"]]],
+        ["gamma", "pi0"],
+        [["gamma", "pi0"]],
+        [[["gamma", "pi0"]]],
+    ],
+)
+def test_create_initial_facts(
+    final_state_groupings,
+    three_body_decay: Topology,
+    particle_database: ParticleCollection,
+):
+    initial_facts = create_initial_facts(
+        three_body_decay,
+        initial_state=[("J/psi(1S)", [-1, +1])],
+        final_state=["gamma", "pi0", "pi0"],
+        particles=particle_database,
+        final_state_groupings=final_state_groupings,
+    )
+    assert len(initial_facts) == 4
+
+
+@pytest.mark.parametrize(
+    "initial_state, final_state",
+    [
+        (["J/psi(1S)"], ["gamma", "pi0", "pi0"]),
+        (["J/psi(1S)"], ["K+", "K-", "pi+", "pi-"]),
+        (["e+", "e-"], ["gamma", "pi-", "pi+"]),
+        (["e+", "e-"], ["K+", "K-", "pi+", "pi-"]),
+    ],
+)
+def test_generate_outer_edge_permutations(
+    initial_state,
+    final_state,
+    three_body_decay: Topology,
+    particle_database: ParticleCollection,
+):
+    initial_state_with_spins = _safe_set_spin_projections(
+        initial_state, particle_database
+    )
+    final_state_with_spins = _safe_set_spin_projections(
+        final_state, particle_database
+    )
+    list_of_permutations = list(
+        _generate_outer_edge_permutations(
+            three_body_decay,
+            initial_state_with_spins,
+            final_state_with_spins,
+        )
+    )
+    n_permutations_final_state = factorial(len(final_state))
+    n_permutations_initial_state = factorial(len(initial_state))
+    n_permutations = n_permutations_final_state * n_permutations_initial_state
+    assert len(list_of_permutations) == n_permutations
+
+
 def test_generate_permutations(
     three_body_decay: Topology, particle_database: ParticleCollection
 ):
@@ -206,3 +213,49 @@ def test_generate_permutations(
     assert spin_permutations[1][2][1] == +1
     assert spin_permutations[2][0][1] == +1
     assert spin_permutations[3][0][1] == +1
+
+
+def test_perform_external_edge_identical_particle_combinatorics(
+    particle_database: ParticleCollection,
+):
+    double_decay = Topology(
+        nodes={0, 1, 2},  # type: ignore
+        edges={
+            0: Edge(originating_node_id=None, ending_node_id=0),
+            1: Edge(originating_node_id=0, ending_node_id=1),
+            2: Edge(originating_node_id=0, ending_node_id=2),
+            3: Edge(originating_node_id=1, ending_node_id=None),
+            4: Edge(originating_node_id=1, ending_node_id=None),
+            5: Edge(originating_node_id=2, ending_node_id=None),
+            6: Edge(originating_node_id=2, ending_node_id=None),
+        },
+    )
+    graph = StateTransitionGraph[ParticleWithSpin](
+        topology=double_decay,
+        edge_props={
+            0: (particle_database["J/psi(1S)"], 0),
+            1: (particle_database["rho(770)0"], 0),
+            2: (particle_database["f(0)(980)"], 0),
+            3: (particle_database["pi-"], 0),
+            4: (particle_database["pi+"], 0),
+            5: (particle_database["pi-"], 0),
+            6: (particle_database["pi+"], 0),
+        },
+        node_props={},
+    )
+    graphs = perform_external_edge_identical_particle_combinatorics(graph)
+    assert len(graphs) == 4
+
+    final_state_edge_ids = graph.topology.outgoing_edge_ids
+    get_final_state = lambda g: tuple(  # noqa: E731
+        g.get_edge_props(i)[0].name for i in final_state_edge_ids
+    )
+    get_originating_node = lambda g: tuple(  # noqa: E731
+        g.topology.edges[i].originating_node_id for i in final_state_edge_ids
+    )
+    for g in graphs:  # pylint: disable=invalid-name
+        assert get_final_state(g) == ("pi-", "pi+", "pi-", "pi+")
+    assert get_originating_node(graphs[0]) == (1, 1, 2, 2)
+    assert get_originating_node(graphs[1]) == (1, 2, 2, 1)
+    assert get_originating_node(graphs[2]) == (2, 1, 1, 2)
+    assert get_originating_node(graphs[3]) == (2, 2, 1, 1)
